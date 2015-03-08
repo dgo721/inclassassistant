dde0d72a096afff2d0f270c6089a1ea2436bcc7d
joaquintrevino <joaquint1991@gmail.com>
2015-03-08 16:57:02 -0600

    Agregué header.php

---
+++ header.php
@@ -0,0 +1,65 @@
+<nav class="navigation-bar">
+<div class="navigation-bar-content">
+    <a href="/" class="element"><span class="icon-grid-view"></span> InClass Assitant </a>
+    <span class="element-divider"></span>
+
+    <a class="pull-menu" href="#"></a>
+    <ul class="element-menu">
+        <li>
+            <a class="dropdown-toggle" href="#">Menú</a>
+            <ul class="dropdown-menu " data-role="dropdown">
+                <li><a href="#">Test</a></li>
+                <li>
+                    <a href="#" class="dropdown-toggle">Test</a>
+                    <ul class="dropdown-menu" data-role="dropdown">
+                        <li><a href="#">Test</a></li>
+                        <li><a href="#">Test</a></li>
+                        <div class="divider"></div>
+                        <li><a href="#">Test</a></li>
+                        <li><a href="#">Test</a></li>
+                        <li><a href="#">Test</a></li>
+                        <li><a href="#">Test</a></li>
+                        <li><a href="#">Test</a></li>
+                    </ul>
+                </li>
+                <li class="divider"></li>
+                <li><a href="#">Test 2</a></li>
+                <li class="disabled"><a href="#">Test 2</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Test 2</a></li>
+            </ul>
+        </li>
+    </ul>
+
+    <div class="no-tablet-portrait">
+        <span class="element-divider"></span>
+        <a class="element brand" href="#"><span class="icon-spin"></span></a>
+        <a class="element brand" href="#"><span class="icon-printer"></span></a>
+        <span class="element-divider"></span>
+
+        <!--<div class="element input-element">
+            <form>
+                <div class="input-control text">
+                    <input type="text" placeholder="Search...">
+                    <button class="btn-search"></button>
+                </div>
+            </form>
+        </div>-->
+
+        <div class="element place-right">
+            <a class="dropdown-toggle" href="#">
+                <span class="icon-cog"></span>
+            </a>
+            <ul class="dropdown-menu place-right" data-role="dropdown">
+                <li><a href="#">Configurar Cuenta</a></li>
+                <li><a href="#">Cerrar Sesión</a></li>
+            </ul>
+        </div>
+        <span class="element-divider place-right"></span>
+
+        <button class="element place-right">
+            <i class="icon-user on-left"></i>Juan López
+        </button>
+    </div>
+</div>
+</nav>
