ef584b5e27be508c55a6c84f04e1b43b8c0f564b
dgo721 <dgo2389@hotmail.com>
2015-03-15 23:54:21 -0600

    dgo-- Functions

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once ('/libs/MysqliDb.php');
+
+function registerUser($registerNo, $name, $password, $type){
+	
+	$db = new MysqliDb ('localhost', 'root', '', 'InClassAssistant');
+
+	$userdata = Array(
+    'registerNo' => $registerNo,
+    'name' => $name,
+    'password' => $db->func('SHA1(?)', Array($password)),
+    'type' => $type
+  );
+
+  $id = $db->insert ('user', $userdata);
+	if ($id)
+		echo 'user was created. Id=' . $id;
+	else
+		echo 'insert failed: ' . $db->getLastError();
+}
+
+ ?>
